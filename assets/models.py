c82a094b5505b086e8daa4d9379b15bdf1b6ae27
Christian <huaxcelogtip@gmail.com>
2026-05-29 17:09:00 +0800

    Remove .env from tracking and add to gitignore

--- assets/models.py
+++ assets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class AssetCategory(models.Model):
+    name        = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name        = 'Asset Category'
+        verbose_name_plural = 'Asset Categories'
+        ordering            = ['name']
+
+    def __str__(self):
+        return self.name
